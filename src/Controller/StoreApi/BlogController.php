a095e81d2f010c24cb08130d24a1575e0e3dc54b
Heiko Bee <hb@maxout.de>
2025-01-22 18:02:46 +0100

    Update BlogController.php

--- src/Controller/StoreApi/BlogController.php
+++ src/Controller/StoreApi/BlogController.php
@@ -85,7 +85,7 @@ class BlogController extends AbstractBlogController
             }
         }
 
-        $criteria->addAssociations(['blogAuthor.salutation', 'blogCategories', 'tags','cmsPage.sections','sections.blocks']);
+        $criteria->addAssociations(['blogAuthor.salutation', 'blogCategories', 'tags','cmsPage.sections','cmsPage.sections.blocks']);
 
         return $criteria;
     }
